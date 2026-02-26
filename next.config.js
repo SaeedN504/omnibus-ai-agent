2c61d7d0e32d9f19f70b946bd450818d28395e34
SaeedN504 <saeed.nazari504@gmail.com>
2026-02-26 20:18:24 +0330

    Fix CSP for WebGL shaders

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; connect-src 'self'; font-src 'self'; object-src 'none'; media-src 'self'; frame-src 'self';"
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
